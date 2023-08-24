9846fdca1c153fc2d919d652f8947973f72fd4fc
Minha <yu6jin@gmail.com>
2023-08-24 17:27:39 +0900

    springboot Mvc + bootstrap, 간단 웹페이지 제작

---
+++ item-service/src/main/java/hello/itemService/domain/item/Item.java
@@ -0,0 +1,21 @@
+package hello.itemService.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//@Data
+@Getter @Setter
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(){}
+
+    public Item(String itemName, Integer price, Integer quantity){
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
