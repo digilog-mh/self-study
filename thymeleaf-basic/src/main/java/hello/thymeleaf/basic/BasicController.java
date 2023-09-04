17c36db5c6b6a62cdf0cb7fb790ad2ef0334c0ca
Minha <yu6jin@gmail.com>
2023-09-04 21:08:24 +0900

    타임리프 - 블록, 자바스크립트 인라인, 템플릿 조각&레이아웃1

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -118,6 +118,18 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("user\"A\"",15));
+        return "basic/javascript";
+    }
+
 
     private void addUsers(Model model){
         List<User> list = new ArrayList<>();
