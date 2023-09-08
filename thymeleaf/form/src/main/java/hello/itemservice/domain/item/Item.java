65b7846815d1224d0a264d81dd91204c68becbeb
Minha <yu6jin@gmail.com>
2023-09-08 22:07:58 +0900

    타임리프 - 타임리프+스프링, 체크박스

--- thymeleaf/form/src/main/java/hello/itemservice/domain/item/Item.java
+++ thymeleaf/form/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,7 +12,7 @@ public class Item {
     private Integer price;
     private Integer quantity;
 
-    private Boolean open; //판매 여부
+    private Boolean open = false; //판매 여부
     private List<String> regions; //등록 지역
     private ItemType itemType; //상품 종류
     private String deliveryCode; //배송 방식식
