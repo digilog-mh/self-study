c08ce63ee6cca46f5ef0e1d78a23b7baecd90136
Minha <yu6jin@gmail.com>
2023-09-12 18:20:45 +0900

    타임리프 - 타임리프+스프링, 라이오 버튼, 셀렉박스

--- thymeleaf/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ thymeleaf/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,7 +1,9 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliberyCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,12 +26,19 @@ public class FormItemController {
 
     @ModelAttribute("regions") // = model.addAttribute("regions", regions);
     public Map<String, String> regions(){
-        Map<String, String> regions = new LinkedHashMap<>(); //linkedHashMap을 사용하는 이유는 넣는 순서를 유지하기 위함. hashMap은 순서 유지가 되지 않음.
+        Map<String, String> regions = new LinkedHashMap<>();
+        //linkedHashMap을 사용하는 이유는 넣는 순서를 유지하기 위함. hashMap은 순서 유지가 되지 않음.
+
         regions.put("seoul","서울");
         regions.put("busan","부산");
         regions.put("jeju","제주");
 
         return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
     }
 
     @GetMapping
@@ -53,11 +63,22 @@ public class FormItemController {
         return "form/addForm";
     }
 
+    @ModelAttribute("deliveryCodes")
+    public List<DeliberyCode> deliberyCodes(){
+        List<DeliberyCode> deliberyCode = new ArrayList<>();
+        deliberyCode.add(new DeliberyCode("FAST", "빠른배송"));
+        deliberyCode.add(new DeliberyCode("NORMAL", "일반배송"));
+        deliberyCode.add(new DeliberyCode("SLOW", "느린배송"));
+
+        return deliberyCode;
+    }
+
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
 
         log.info("item.open={}",item.getOpen());
         log.info("item.regions={}",item.getRegions());
+        log.info("item.itemType={}",item.getItemType());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
