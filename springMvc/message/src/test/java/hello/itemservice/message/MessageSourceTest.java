d467ff8422002a0274a6cd73aefa72bf7581cb65
Minha <yu6jin@gmail.com>
2023-09-14 19:55:06 +0900

    spring boot, Mvc - 메세징 및 국제화

---
+++ springMvc/message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,21 @@
+package hello.itemservice.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessage() {
+        String result = ms.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+}
