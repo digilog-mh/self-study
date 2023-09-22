acdb47db2fa6cab10bfce0b7c18bd44f07579523
Minha <yu6jin@gmail.com>
2023-09-22 16:50:46 +0900

    spring Boot Mvc - 검증처리4,5

--- springMvc/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ springMvc/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -118,13 +119,56 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())){
+            bindingResult.rejectValue("itemName","required");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() >1000000){
+            bindingResult.rejectValue("price","range", new Object[]{1000,1000000}, null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999){
+            bindingResult.rejectValue("quantity","max", new Object[]{9999},null);
+        }
+
+        //특정 필드가 아닌 복함 룰 검증
+        if (item.getPrice()!= null && item.getQuantity() != null){
+            int result = item.getPrice() * item.getQuantity();
+            if (result < 10000){
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"}, new Object[]{10000,result},null));
+                bindingResult.reject("totalPriceMin", new Object[]{10000, result}, null);
+            }
+        }
+
+        //검증에 실패시, 다시 입력폼으로.
+        if (bindingResult.hasErrors()){
+            log.info("errors = {}",bindingResult);
+            //model.addAttribute("bindingResult", bindingResult);
+            //BindingResult는 뷰에 자동으로 넘어가기에 Model에 담지 않아도 된다.
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        log.info("objectName={}",bindingResult.getObjectName());
+        log.info("target={}",bindingResult.getTarget());
+
+        //검증 로직
+        //ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult,"itemName","required");
+
+        if (!StringUtils.hasText(item.getItemName())){
             bindingResult.addError(new FieldError("item","itemName", item.getItemName(), false ,new String[]{"required.item.itemName"}, null,null));
         }
+
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() >1000000){
             bindingResult.addError(new FieldError("item","price",item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
         }
---
+++ springMvc/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,29 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required","item");
+        for (String messageCode : messageCodes){
+            System.out.println("messageCode = "+messageCode);
+        }
+        Assertions.assertThat(messageCodes).containsExactly("required.item","required");
+    }
+
+    @Test
+    void messageCodesresolberField(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = "+messageCode);
+        }
+    }
+}
