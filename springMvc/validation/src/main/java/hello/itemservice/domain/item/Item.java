85e1c28554b44f8f72d7e1382bb7f5d8c68e839f
Minha <yu6jin@gmail.com>
2023-09-26 17:23:36 +0900

    spring Boot Mvc - Bean Validator 1,2

--- springMvc/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ springMvc/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,27 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백x")
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 10000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
