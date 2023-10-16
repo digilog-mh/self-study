e9ba231cf259542a000ee1c5496ebe03444c8e8c
Minha <yu6jin@gmail.com>
2023-10-16 19:58:56 +0900

    Spring Boot - 서블릿 필터 추가

---
+++ springMvc/login/src/main/java/hello/login/domain/login/WebConfig.java
@@ -0,0 +1,22 @@
+package hello.login.domain.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+    }
+}
