e3effa759adea3cc3e7afc4ac21d0b3cfb746ad4
Minha <yu6jin@gmail.com>
2023-10-18 23:11:04 +0900

    Spring Boot - 로그인, argument

--- springMvc/login/src/main/java/hello/login/domain/login/WebConfig.java
+++ springMvc/login/src/main/java/hello/login/domain/login/WebConfig.java
@@ -2,18 +2,25 @@ package hello.login.domain.login;
 
 import hello.login.web.Interceptor.LogInterceptor;
 import hello.login.web.Interceptor.LoginCheckInterceptor;
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
