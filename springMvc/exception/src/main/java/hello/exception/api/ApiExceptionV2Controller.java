1d821ff0949a2c6db2c31c974d731ad880202903
Minha <yu6jin@gmail.com>
2023-10-31 15:55:18 +0900

    Springboot - ExceptionResolver, @ExceptionHandler

---
+++ springMvc/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -0,0 +1,62 @@
+package hello.exception.api;
+
+import hello.exception.exHandler.ErrorResult;
+import hello.exception.exception.UserException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class ApiExceptionV2Controller {
+
+    //Controller에서 Exception 발생시, Dispatcher servlet이 exceptionResolver 호출.
+    //exceptionResolver에서 해당 Controller에서 @ExceptionHandler 어노테이션을 조회 후 호출.
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e){
+        log.error("[exceptionHandler] ex",e);
+        return new ErrorResult("BAD", e.getMessage());
+        //정상처리.
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e){
+        log.error("[exceptionHandler] ex ",e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+        //user-ex
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e){
+        log.error("[exceptionHandler] ex ",e);
+        return new ErrorResult("EX", "내부올류");
+        //ex
+    }
+
+    @GetMapping("/api2/members/{id}")
+    public MemberDto getMember(@PathVariable("id")String id){
+        if (id.equals("ex")){
+            throw new RuntimeException("잘못된 사용자");
+        }
+        if(id.equals("bad")){
+            throw new IllegalArgumentException("잘못된 입력값");
+        }
+        if (id.equals("user-ex")){
+            throw new UserException("사용자 오류");
+        }
+        return new MemberDto(id,"hello "+id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static  class MemberDto{
+        private String memberId;
+        private String name;
+    }
+}
--- springMvc/exception/src/main/java/hello/exception/exception/BadRequestException.java
+++ springMvc/exception/src/main/java/hello/exception/exception/BadRequestException.java
@@ -5,6 +5,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 오류-badRequestException")
 public class BadRequestException extends RuntimeException{
-
-
 }
