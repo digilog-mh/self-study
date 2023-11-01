245a1d41c360a5bef0ebe34cb495ea17cd401580
Minha <yu6jin@gmail.com>
2023-11-01 21:28:39 +0900

    Node.Js - url이해, 동적 웹페이지

--- nodeJs/helloWorld/main.js
+++ nodeJs/helloWorld/main.js
@@ -1,16 +1,44 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
+    var _url = request.url;
+    var quertyData = url.parse(_url,true).query;
+    var title = quertyData.id;
     if(request.url == '/'){
-        url = '/index.html';
+        title = 'welcome';
     }
     if(request.url == '/favicon.ico'){
         return response.writeHead(404);
     }
+
     response.writeHead(200);
-    console.log(__dirname+url);
-    response.end(fs.readFileSync(__dirname + url));
+
+    var template = `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+      <img src="coding.jpg" width="100%">
+      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+      </p>
+    </body>
+    </html>
+    `;
+
+    response.end(template);
 
 });
 app.listen(3000);
