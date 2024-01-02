4e04dac1f332e3904f12cc64f1702a03fcec8407
Minha <yu6jin@gmail.com>
2024-01-02 20:38:21 +0900

    Kotlin - 반복문의 label, return

---
+++ Kotlin/src/ch4.kt
@@ -0,0 +1,57 @@
+import hi.pkg.*
+
+class ch4 {
+}
+
+//암시적 레이블 사용.
+fun foo4(){
+    var nums = listOf(0,1,2,3)
+    nums.forEach{
+        if (it == 1) return@forEach
+        print(it)
+    }
+
+    println("--4--")
+}
+
+//레이블 사용.
+fun foo3(){
+    var nums = listOf(0,1,2,3)
+    nums.forEach label@ {
+                    if (it == 1) return@label
+                    print(it)
+                }
+
+    println("--3--")
+}
+
+fun foo2(){
+    var nums = listOf(0,1,2,3)
+    nums.forEach(
+        { //람다식
+            if (it==1) return
+            print(it)
+        }
+    )
+    println("---")
+}
+
+fun foo1(){
+    var nums = listOf(0,1,2,3)
+    nums.forEach(
+        fun (v: Int){
+            if (v == 1) return
+            print(v)
+        }
+    )
+    println("")
+}
+
+fun main() {
+    //printOther2()
+    //foo1()
+    //foo2()
+    //foo3()
+    foo4()
+}
+
---
+++ Kotlin/src/ch4_2.kt
@@ -0,0 +1,8 @@
+package hi.pkg
+
+class ch4_2 {
+}
+
+fun printOther2(){
+    println("==other package==")
+}
