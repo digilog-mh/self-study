f1d9f7cbb97a6160e2dcc209614658cc78397d72
Minha <yu6jin@gmail.com>
2024-01-22 17:50:33 +0900

    Maven project & JPA - 객체, 테이블 매칭 & DB schema 자동 생성

--- Spring-JPA/jpaBasic/src/main/java/hellojpa/JpaMain.java
+++ Spring-JPA/jpaBasic/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
+
 
 public class JpaMain {
     public static void main(String[] args) {
--- Spring-JPA/jpaBasic/src/main/java/hellojpa/Member.java
+++ Spring-JPA/jpaBasic/src/main/java/hellojpa/Member.java
@@ -1,13 +1,18 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 public class Member {
+
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10)
     private String name;
+
 
     public Member(Long id, String name) {
         this.id = id;
@@ -33,4 +38,5 @@ public class Member {
     public void setName(String name) {
         this.name = name;
     }
+
 }
